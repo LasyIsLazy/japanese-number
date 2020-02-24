94b1bf3829e20fe5dabe54fc4fe0fc2f48bc9026
Lasy <1255352506@qq.com>
2020-02-24 16:51:41 +0800

    基本逻辑

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  extends: ['plugin:vue/essential', '@vue/airbnb', '@vue/typescript/recommended'],
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+  env: {
+    browser: true,
+    es6: true,
+  },
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  plugins: ['vue', '@typescript-eslint'],
+  rules: {
+    'no-unused-vars': 1,
+    semi: [1, 'never'],
+  },
+}
--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,52 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <div>{{curNum}}</div>
+    <div v-if="showAnswer">{{curJapanese}}</div>
+    <div @click="handleClick">查看答案</div>
+    <div @click="next">切换</div>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import { Component, Vue } from 'vue-property-decorator'
+import HelloWorld from './components/HelloWorld.vue'
+
+enum Dictionary {
+  'れい、ぜろ' = 0,
+  'いち' = 1,
+  'に' = 2,
+  'さん' = 3,
+  'し、よん' = 4,
+  'ご' = 5,
+  'ろく' = 6,
+  'しち、なな' = 7,
+  'はち' = 8,
+  'く、きゅう' = 9,
+}
 
 @Component({
   components: {
     HelloWorld,
   },
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+  private curNum = 1
+
+  private get curJapanese() {
+    return Dictionary[this.curNum]
+  }
+
+  private showAnswer = false
+
+  private handleClick() {
+    this.showAnswer = true
+  }
+
+  private next() {
+    this.showAnswer = false
+    this.curNum = Math.floor(Math.random() * 10)
+  }
+}
 </script>
 
 <style>
